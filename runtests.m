811a5ab8d120750fccef0102a7766db012e8583a
Vladimir Perić <vlada.peric@gmail.com>
2013-03-28 20:42:20 +0100

    runtests: Add return value to runtests.m
    Calling run(TestSuite) returns a TestResult object. Change the runtests
    function to also return this argument, so we can inspect it later.
    
    Also display the percentage of passing tests.

--- runtests.m
+++ runtests.m
@@ -1,13 +1,15 @@
-function runtests(what)
+function r = runtests(what)
     import matlab.unittest.TestSuite;
     addpath('mmockito');
 
     switch what,
         case 'acceptance',
-            run(TestSuite.fromFolder('tests\acceptance'))
+            r = run(TestSuite.fromFolder('tests\acceptance'));
         case 'unit',
-            run(TestSuite.fromFolder('tests\unit'))
+            r = run(TestSuite.fromFolder('tests\unit'));
         otherwise,
             error('Do not know what to run.');
     end
+    
+    disp([num2str(nnz([r.Passed]) / numel(r) * 100), ' % tests passed']);
 end
