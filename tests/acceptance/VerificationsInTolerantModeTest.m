2056a34e254da967c89701d9e84dee3c8888b11d
Petr Pošík <petr.posik@gmail.com>
2013-04-22 10:16:15 +0200

    Verifications tests added.

--- tests/acceptance/VerificationsInTolerantModeTest.m
+++ tests/acceptance/VerificationsInTolerantModeTest.m
@@ -1,11 +1,11 @@
-classdef VerificationsTest < matlab.unittest.TestCase
-%VerificationsTest Acceptance tests for the mock verification behavior.
+classdef VerificationsInTolerantModeTest < matlab.unittest.TestCase
+%VerificationsInTolerantModeTest Acceptance tests for the mock verification behavior.
     
     properties
     end
     
     methods (Test)
-        function tolerant_simpleVerification(testCase)
+        function simpleVerification(testCase)
             % Given
             m = Mock('tolerant');
             % When
@@ -14,10 +14,90 @@ classdef VerificationsTest < matlab.unittest.TestCase
             % Then
             m.verify.setParam(10);
             m.verify.goHome();
-            testCase.assertError(@() m.verify.isThereTheGod(), @MException);
+            testCase.assertError(@() m.verify.isThereTheGod(), ?VerificationError);
         end
         
-
+        function verificationOnRealObject_success(testCase)
+            % Given
+            m = Mock(RealClass, 'tolerant');
+            % When
+            x = m.get(10);
+            % Then
+            m.verify.get(10);
+        end
+        
+        function verificationOnRealObject_failure(testCase)
+            % Given
+            m = Mock(RealClass, 'tolerant');
+            % When
+            % Then
+            testCase.assertError(@() m.verify.get(10), ?VerificationError);
+        end
+        
+        function numberOfInvocations(testCase)
+            % Given
+            m = Mock('tolerant');
+            % When
+            m.one();
+            m.two(pi);
+            m.two(pi);
+            % Then
+            m.verify.one().times(1);
+            m.verify.one(AnyArgs).times(1);
+            m.verify.one().atLeast(0);
+            m.verify.one().atLeast(1);
+            testCase.assertError(@() ...
+                m.verify.one().atLeast(2), ...
+                ?VerificationError);
+            testCase.assertError(@() ...
+                m.verify.one().atMost(0), ...
+                ?VerificationError);
+            m.verify.one().atMost(1);
+            m.verify.one().atMost(2);
+            m.verify.one(1).times(0);
+            testCase.assertError(@()...
+                m.verify.one(pi).times(1), ...
+                ?VerificationError);
+            testCase.assertError(@() ...
+                m.verify.one().never(), ...
+                ?VerificationError);
+            
+            m.verify.two(pi).times(2);
+            m.verify.two(AnyArgs).times(2);
+            m.verify.zero().times(0);
+            m.verify.zero().never();
+        end
+        
+        function verificationInOrder_success_onSingleMock(testCase)
+            % Given
+            m = Mock('tolerant');
+            % When
+            m.first();
+            m.second();
+            % Then
+            m.verifyInOrder();
+            m.verify.first();
+            m.verify.second();
+        end
+        
+        function verificationInOrder_failure_onSingleMock(testCase)
+            % Given
+            m = Mock('tolerant');
+            % When
+            m.first();
+            m.second();
+            % Then
+            m.verifyInOrder;
+            testCase.assertError(@()...
+                m.verify.second(), ?VerificationError);
+        end
+        
+        function verificationInOrder_onMultipleMocks(testCase)
+            % TODO
+        end
+        
+        % TODO: mock.verifyZeroInteractions
+        % TODO: mock.verifyNoMoreInteractions
     end
     
 end
